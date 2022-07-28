724cc86ebea47eb7e5c0bf573ea98dd7069c9a7e
leonard33 <leonardkombo14@gmail.com>
2022-07-28 11:01:55 +0300

    test fizzbuzz

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -13,6 +13,23 @@ RSpec.describe 'Solver' do
     end
   end
 
+  context 'should handle fizzbuzz function' do
+    it 'should return fizz when number is divisible by 3' do
+      expect(@solver.fizzbuzz(9)).to eq 'fizz'
+    end
+    it 'should return buzz when number is divisible by 5' do
+      expect(@solver.fizzbuzz(5)).to eq 'buzz'
+    end
+    it 'should return fizzbuzz when number is divisible by 3 and 5' do
+      expect(@solver.fizzbuzz(90)).to eq 'fizzbuzz'
+    end
+
+    it 'should return number if not divisible by 3 and 5' do
+      expect(@solver.fizzbuzz(13)).to eq '13'
+    end
+
+  end
+
   context 'should handle reverse function' do
     it 'should return the reversed word' do
       expect(@solver.reverse("John")).to eq 'nhoJ'
