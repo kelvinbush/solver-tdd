0565dfc745a5b258f141483d5697d9c241fd362e
Kelvin Bush <kelybush@gmail.com>
2022-07-28 11:33:21 +0300

    fix: add a missing test case

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,6 +8,10 @@ RSpec.describe 'Solver' do
       expect(@solver.factorial(6)).to eq 720
     end
 
+    it 'should return 1 if 0 is given' do
+      expect(@solver.factorial(0)).to eq 1
+    end
+
     it 'should throw error when negative is given' do
       expect { @solver.factorial(-7) }.to raise_error(RangeError, 'A negative number is not allowed')
     end
